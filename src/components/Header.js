7a6ec4098df1b30bfdf4f9617460312570e01d63
trunglive <trunglive@gmail.com>
2018-05-21 14:24:47 -0400

    style(view): add styling for project dashboard

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,37 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => (
-  <div>
-    this is the header
+  <div className="header">
+    <div className="header-left">
+      <p className="title">Projects</p>
+      <div className="header__tab">
+        <Link to="/projects/dashboard">
+          <div className="header__tab--dashboard">Dashboard</div>
+        </Link>
+        <Link to="/projects/list">
+          <div className="header__tab--all-projects">All Projects</div>
+        </Link>
+      </div>
+    </div>
+    <div className="header-right">
+      <div className="client-or-project">
+      <img className="search-icon" src="/icons/search.svg" />
+      <span>Client or Projects</span>
+      </div>
+      <div className="manage-clients">
+      <img className="person-icon" src="/icons/person.svg" />
+      <span>Manage clients</span>
+      </div>
+      <div className="new-project">
+        <img
+          className="new-project__plus-sign-icon"
+          src="/icons/plus-sign.svg"
+        />
+        <span>New project</span>
+      </div>
+    </div>
   </div>
-)
+);
 
 export default Header;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,9 +5,9 @@ class Sidebar extends Component {
     return (
       <div className="sidebar">
         <div className="sidebar__initial">T</div>
-        <div className="sidebar__hr"></div>
+        <div className="sidebar__hr" />
         <div className="sidebar__hours">
-          <img className="sidebar__icon" src="/icons/hours.svg" />
+          <img className="sidebar__icon disable" src="/icons/hours.svg" />
           <p>hours</p>
         </div>
         <div className="sidebar__projects">
@@ -15,11 +15,11 @@ class Sidebar extends Component {
           <p>projects</p>
         </div>
         <div className="sidebar__people">
-          <img className="sidebar__icon" src="/icons/people.svg" />
+          <img className="sidebar__icon disable" src="/icons/people.svg" />
           <p>people</p>
         </div>
         <div className="sidebar__reports">
-          <img className="sidebar__icon" src="/icons/reports.svg" />
+          <img className="sidebar__icon disable" src="/icons/reports.svg" />
           <p>reports</p>
         </div>
         <div className="sidebar__trial-left">
@@ -28,16 +28,22 @@ class Sidebar extends Component {
         </div>
         <div className="sidebar__getting-started-percentage">85%</div>
         <div className="sidebar__settings">
-          <img className="sidebar__icon" src="/icons/settings.svg" />
+          <img className="sidebar__icon disable" src="/icons/settings.svg" />
         </div>
         <div className="sidebar__notifications">
-          <img className="sidebar__icon" src="/icons/notifications.svg" />
+          <img
+            className="sidebar__icon disable"
+            src="/icons/notifications.svg"
+          />
         </div>
         <div className="sidebar__support">
-          <img className="sidebar__icon" src="/icons/support.svg" />
+          <img className="sidebar__icon disable" src="/icons/support.svg" />
         </div>
         <div className="sidebar__personal-settings">
-          <img className="sidebar__icon" src="/icons/personal-settings.svg" />
+          <img
+            className="sidebar__icon disable"
+            src="/icons/personal-settings.svg"
+          />
         </div>
       </div>
     );
