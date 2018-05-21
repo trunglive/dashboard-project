d7c544f651493e9302ab6e1251832c87df3b21d2
trunglive <trunglive@gmail.com>
2018-05-21 12:38:48 -0400

    style(view): add styling for sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,45 @@ import React, { Component } from "react";
 
 class Sidebar extends Component {
   render() {
-    return <div className="sidebar">this is the sidebar</div>;
+    return (
+      <div className="sidebar">
+        <div className="sidebar__initial">T</div>
+        <div className="sidebar__hr"></div>
+        <div className="sidebar__hours">
+          <img className="sidebar__icon" src="/icons/hours.svg" />
+          <p>hours</p>
+        </div>
+        <div className="sidebar__projects">
+          <img className="sidebar__icon" src="/icons/projects.svg" />
+          <p>projects</p>
+        </div>
+        <div className="sidebar__people">
+          <img className="sidebar__icon" src="/icons/people.svg" />
+          <p>people</p>
+        </div>
+        <div className="sidebar__reports">
+          <img className="sidebar__icon" src="/icons/reports.svg" />
+          <p>reports</p>
+        </div>
+        <div className="sidebar__trial-left">
+          <p>12 days</p>
+          <p>left in trial</p>
+        </div>
+        <div className="sidebar__getting-started-percentage">85%</div>
+        <div className="sidebar__settings">
+          <img className="sidebar__icon" src="/icons/settings.svg" />
+        </div>
+        <div className="sidebar__notifications">
+          <img className="sidebar__icon" src="/icons/notifications.svg" />
+        </div>
+        <div className="sidebar__support">
+          <img className="sidebar__icon" src="/icons/support.svg" />
+        </div>
+        <div className="sidebar__personal-settings">
+          <img className="sidebar__icon" src="/icons/personal-settings.svg" />
+        </div>
+      </div>
+    );
   }
 }
 
