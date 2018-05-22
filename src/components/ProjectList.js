8061a81e46ff20b36ee3cf31dc44a786ef9feaf3
trunglive <trunglive@gmail.com>
2018-05-22 14:32:43 -0400

    feat(view): add markup for all-projects tab

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -4,7 +4,13 @@ class ProjectList extends Component {
   render() {
     return (
       <div className="project-list">
-        <div>This is the page of project list</div>
+        <div className="project-list__title">
+          <p>Name</p>
+          <p>Logged hours</p>
+          <p>Budget</p>
+          <p>Budget spent</p>
+          <p>Budget left</p>
+        </div>
       </div>
     );
   }
